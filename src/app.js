86227a12385b708954ee6b6f6212846af7b4fc68
v1rtuxx <109669127+v1rtuxx@users.noreply.github.com>
2024-08-04 17:05:00 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const tvShowsRouter = require('./tv_shows');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -23,6 +25,9 @@ const fetchDataWithRetry = async (url, attempts, delay) => {
     }
     return null;
 };
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/tv_shows', tvShowsRouter);
 
